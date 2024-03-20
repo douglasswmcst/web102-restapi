0224d6ec16d4e87df3cd2761dcac020f4bd484a3
douglasswmcst <douglassim.cst@rub.edu.bt>
2024-03-20 10:00:32 +0600

    fix: typescript runtime error for .ts files

--- src/seed.js
+++ src/seed.js
@@ -2,9 +2,39 @@ import { PrismaClient, Prisma } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-const user = await prisma.user.create({
-  data: {
-    email: "elsa@prisma.io",
-    name: "Elsa Prisma",
-  },
-});
+// 1. CREATE USER
+async function createUser(_email, _name) {
+  await prisma.user.create({
+    data: {
+      email: _email,
+      name: _name,
+    },
+  });
+}
+console.log(createUser("douglas@gmail.com", "Douglas"))
+
+// 2. CREATE USER PROFILE
+
+async function createUserProfile(_email, _name) {
+  await prisma.user.create({
+    data: {
+      email: _email,
+      name: _name,
+    },
+  });
+}
+console.log(createUserProfile("douglas@gmail.com", "Douglas"))
+
+// 3. CREATE POST
+
+// 4. READ USER PROFILE
+
+// 5. READ ALL POSTS (FEED)
+
+// 6. READ A SPECIFIC POST
+
+// 7. UPDATE USER PROFILE
+
+// 8. UPDATE A SPECIFIC POST
+
+// 9. DELETE A SPECIFIC POST
